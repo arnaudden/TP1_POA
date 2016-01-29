3a26c7df3d9c4e3b86ac5646276f85fdb4b7da3f
ganujan <anu_93@hotmail.fr>
2016-01-29 12:17:52 -0500

    Class Commande

--- src/Commande.java
+++ src/Commande.java
@@ -22,7 +22,7 @@ public class Commande implements Serializable {
 	// Chargement
 	String nomQualifieDeClasse;
 	// Creation 
-	String nomDeClass;
+	String nomDeClasse;
 	// Creation & lecture & ecriture
 	String identificateur;
 	//Lecture & ecriture 
@@ -33,17 +33,26 @@ public class Commande implements Serializable {
 	public Commande(String line){
 		int indice = line.indexOf("#");
 		String fonction = line.substring(0, indice);
-		System.out.println(fonction);
-		
+		//System.out.println(fonction);
+		fct = fonction;
+		String tmp = new String();
 		switch (fonction){
 		
 		case "compilation": break;
 		
-		case "chargement": break;
+		case "chargement": nomQualifieDeClasse = line.substring(indice + 1); 
+			break;
 		
-		case "creation": break;
+		case "creation": tmp = line.substring(indice + 1);
+		indice = tmp.indexOf("#");
+		nomDeClasse = tmp.substring(0, indice);
+		identificateur = tmp.substring(indice +1);
+			break;
 		
-		case "lecture": System.out.println("demande de lecture");
+		case "lecture": tmp = new String(line.substring(indice + 1));
+		indice = tmp.indexOf("#");
+		identificateur = tmp.substring(0, indice);
+		nomAttribut = tmp.substring(indice +1);
 			break;
 			
 		case "ecriture": break;	
@@ -52,12 +61,37 @@ public class Commande implements Serializable {
 		default: System.out.println("Fonction Inconnue");
 		}
 	}
+	
+	public String toString(){
+		
+		String result = new String();
+		result = "Fonctison: "+fct;
+		switch (fct){
+				
+				case "compilation": break;
+				
+				case "chargement":  result += ", nom qualifie de classe: "+ nomQualifieDeClasse;
+					break;
+				
+				case "creation": result += ", nom de classe: "+ nomDeClasse+", identificateur: "+ identificateur;
+					break;
+				
+				case "lecture": result += ", identificateur: "+ identificateur+", nom d'attribut: "+nomAttribut;
+					break;
+					
+				case "ecriture": break;	
+				
+				case "fonction": break;
+				default: System.out.println("Fonction Inconnue");
+				}
+		return result;
+	}
 
 	public static void main(String argv[]) throws Exception 
     { 
-		String txt = "lecture#texte";
+		String txt = "ecriture#8inf853#titre#Architecture des applications";
 		Commande cmd = new Commande(txt);
-		
+		System.out.println(cmd.toString());	
     }
 	
 }
